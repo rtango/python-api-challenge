a0bbad4cc8f0b553870edc7628a41f81800438de
Richard Tango <rjtango10@gmail.com>
2020-09-13 18:25:43 -0400

    Add to existing folder

--- WeatherPy/starter_code/api_keys.py
+++ WeatherPy/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "cef8ec131b1cfcd3d55ebfd2316ad1da"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCsymVuazZpGXsXZpzidLBtCE0rurmwDkw"
